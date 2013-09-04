f4a8858f968d6c6e77c792347804270f3e765bbe
Josh Olson <jobbque@gmail.com>
2013-09-04 11:54:56 -0700

    Visual update to Version2 of HotCold

--- HotCold/Version2/application.js
+++ HotCold/Version2/application.js
@@ -17,54 +17,26 @@ function getStatus (difference) {
 	return status;
 };
 
+function highLow (theirNumber) {
+	if (theirNumber > theRandomNumber) {
+		highLowStatus = "Too High";
+	} else if (theirNumber < theRandomNumber) {
+		highLowStatus = "Too Low";
+	} else {
+		highLowStatus = "You Got It!"
+	}
+	return highLowStatus;
+}
+
 function testNumber() {
 	
 	var theirNumber = document.getElementById('theNumber').value;
 	var difference = Math.abs(theRandomNumber - theirNumber);
 	var status = "cold";
-	var highLow = "higher";
+	var highLowStatus = "Too High";
 
-	document.getElementById("randNumber").innerHTML= "The Random Number is: " + theRandomNumber;
+	//document.getElementById("randNumber").innerHTML= "The Random Number is: " + theRandomNumber;
 	//document.getElementById("aNumber").innerHTML= "Their Number is: " + theirNumber;
-	document.getElementById("numberDifference").innerHTML= "Difference: " + difference;
-	document.getElementById("currentStatus").innerHTML= "Current Status: " + getStatus(difference);	
+	//document.getElementById("numberDifference").innerHTML= "Difference: " + difference;
+	document.getElementById("currentStatus").innerHTML= highLow(theirNumber);	
 };
-
-
-/*	var theirNumber = 0
-	//var theirNumber=prompt("Please Enter a Number")
-		while (theRandomNumber != theirNumber) {
-			theirNumber = document.getElementById('theNumber').value
-			var difference = Math.abs(theRandomNumber - theirNumber)
-			var status = "cold"
-
-			function getStatus (difference) {
-				if (difference == 0) {
-					status = "You Got It!"
-				} else if (difference > 20) {
-					status = "Cold! Try Again."
-				} else if (difference <= 20 && difference >= 10) {
-					status = "Warm! Try Again."
-				} else {
-					status = "Hot! Try Again."
-				} 
-				return status;
-			}
-			document.getElementById("randNumber").innerHTML= "The Random Number is: " + theRandomNumber
-			document.getElementById("theNumber").innerHTML= "Their Number is: " + theirNumber
-			document.getElementById("numberDifference").innerHTML= "Difference: " + difference
-			document.getElementById("currentStatus").innerHTML= "Current Status: " + getStatus(difference)
-		}
-}
-
-/*function getRandom(number, count) {
-	var currentRandomNumber = number
-	var	counter = count
-
-	if (counter == 0) {
-		newRandomNumber = Math.floor(Math.random()*101)
-		return newRandomNumber
-	} else {
-		return someNumber;
-	}
-}*/
