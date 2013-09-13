f6f5ea604b9ec8ab78dfc55277f9ede6a72913ff
Josh Olson <jobbque@gmail.com>
2013-09-13 14:52:17 -0700

    First Draft of Shopping List App

---
+++ Shopping/application.js
@@ -0,0 +1,66 @@
+var shoppingList = new Array();
+var itemNumber = 0;
+
+$(document).ready(function() {
+	$("#newItem").keyup(function(){
+    	if(event.keyCode == 13){
+        	$("#button").click();
+    	}
+	});
+});
+
+function manageShoppingList () {
+
+	$("#shoppingList").empty();
+
+	function createShoppingList () {
+		for (var i=0; i < shoppingList.length; i++) {
+			if (typeof shoppingList[i] != 'undefined') {
+				$("#shoppingList").append("<input type='checkbox' id='checkbox"+i+"'><span id='shoppingListItem"+i+"'>"+shoppingList[i]+"</span> <br>");
+			}
+		}
+	}
+
+	createShoppingList ();
+		
+	//If you click anywhere on the shoppingList form, a check is done to see which of the checkboxes for the form
+	//elements are checked, if a checkbox is checked, then the corresponding item to that checkbox is lined through
+		
+	$("#shoppingList").click(function() {
+		for (var i=0; i < shoppingList.length; i++) {
+			if ($('#checkbox'+i).is(':checked')) {
+    			$("#shoppingListItem"+i).css ("text-decoration", "line-through");
+			} else {
+				$("#shoppingListItem"+i).css ("text-decoration", "none");
+			}
+		}
+	});
+
+	$("#clearCheckedItems").click(function() {
+
+		for (var i=0; i < shoppingList.length; i++) {
+			if ($('#checkbox'+i).is(':checked')) {
+    			delete shoppingList[i];
+			}
+		}
+		$("#shoppingList").empty();
+    	createShoppingList ();
+		//$("#justTesting").text(shoppingList);
+	});
+}
+
+//Takes and item as imput from the form, adds it to the array
+//sends array contents to screen.
+
+function addItemToList () {
+
+	$("#newItem").text(function(){
+    	this.select();
+	});
+
+	var Item = document.getElementById('newItem').value;
+	shoppingList[itemNumber] = Item;
+	itemNumber++;
+
+	manageShoppingList ();
+}
